3a65c0470d685ef9bae6302229b8a0875ed0d568
Cristiano Moraes Bilacchi Azarias <cristiano@mini.local>
2023-08-06 01:15:10 -0300

    logistic + cumulative

--- analysis.py
+++ analysis.py
@@ -25,7 +25,7 @@ for iResp in blocks[357.1429].groupby('Delta'):
     resp2[round(iResp[0])] = len(iResp[1]['Accuracy'])
 
 # %%
-df = pd.read_csv('data/P005.csv')
+df = pd.read_csv('data/D001.csv')
 plt.rcParams['font.size'] = 8
 X = df[['staircase.intensity', 'staircase.response']].dropna().values[:,0]
 Y = df[['staircase.intensity', 'staircase.response']].dropna().values[:,1]
@@ -47,6 +47,12 @@ Y = np.array(list(map(lambda x: sum(x), splits)))
 N = np.array(list(map(lambda x: len(x), splits)))
 
 #%%
+def logistic_cdf(x, mu, sigma):
+    return 1 / (sigma * (1 + pm.math.exp(-(x-mu)/sigma))**2)
+
+def norm_cdf(x, mu, sigma):
+    return 0.5 * (1 + pm.math.erf((x - mu)/(sigma * pm.math.sqrt(2))))
+
 with pm.Model() as model:
     x_hat = pm.MutableData('x_hat', X)
     y_hat = pm.MutableData('y_hat', Y)
@@ -54,20 +60,23 @@ with pm.Model() as model:
    
     mu = pm.Laplace('mu', 0, 0.1, shape=(2,))
     sigma = pm.Uniform('sigma', 0, 1000, shape=(2,))
-    gamma = pm.Uniform('gamma', 0, 1)
-    lmbda = pm.Uniform('lmbda', 0, 1)
     alpha = pm.Normal('alpha', mu[0], sigma[0], shape=(1,))
     beta = pm.Normal('beta', mu[1], sigma[1], shape=(1,))
 
-    theta = pm.Deterministic('theta', gamma + (1 - gamma - lmbda) * pm.math.exp(pm.Normal.logcdf(alpha * x_hat + beta, 0, 1)))
+    #theta = pm.Deterministic('theta', pm.math.exp(pm.Normal.logcdf(alpha * x_hat + beta, 0, 1)))
+    #theta = pm.Deterministic('theta', pm.invlogit(alpha * x_hat + beta))
+    theta = pm.Deterministic('theta', norm_cdf(alpha * x_hat + beta, 0, 1))
 
     likelihood = pm.Binomial('likelihood', n=n, p=theta, observed=y_hat)
 
 with model:
-    idata = pm.sample(30000, tune=5000, target_accept=.99)
+    idata = sample_numpyro_nuts(10000, tune=5000, target_accept=.99)
+    #idata = pm.sample(30000, tune=5000, target_accept=.99)
 
 #%%
-PS = lambda x, a, b, g, l: g + (1 - g - l) * ss.norm().cdf(x*a + b)
+#PS = lambda x, a, b, g, l: g + (1 - g - l) * ss.norm().cdf(x*a + b)
+PS = lambda x, a, b: ss.norm().cdf(x * a + b)
+#PS = lambda x, a, b: ss.logistic().cdf(x * a + b)
 
 popt, pcov = curve_fit(PS, X, Y/N)
 
@@ -77,14 +86,15 @@ plt.scatter(X, Y/N)
 
 alpha = idata.posterior.alpha.mean(['draw', 'chain']).values
 beta = idata.posterior.beta.mean(['draw', 'chain']).values
-gamma = idata.posterior.gamma.mean(['draw', 'chain']).values
-lmbda = idata.posterior.lmbda.mean(['draw', 'chain']).values
+#gamma = idata.posterior.gamma.mean(['draw', 'chain']).values
+#lmbda = idata.posterior.lmbda.mean(['draw', 'chain']).values
 
-plt.plot(t, PS(t, alpha, beta, gamma, lmbda))
+plt.plot(X, idata.posterior.theta.mean(['chain', 'draw']).values)
+plt.plot(t, PS(t, alpha, beta), linestyle='dashed')
 
 #%%
 theta = np.linspace(0,1,100)
 for iX, iN, iY in zip(X, N, Y):
     plt.fill_betweenx(theta, iX, iX-ss.binom.pmf(k=iY, n=iN, p=theta)*0.1)
-plt.plot(t, PS(t, alpha, beta))
-plt.plot(t, PS(t, *popt))
+plt.plot(t, PS(t, alpha, beta), linestyle='dashed', color='grey')
+plt.plot(t, PS(t, *popt), linestyle='dashed', color='k')
